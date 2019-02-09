e2a570b66f694d9e57b2a11825d7d8e0b17cff8d
Mitch McAffee <themcaffee@gmail.com>
2019-02-09 06:03:16 -0700

    Add documentation to frontend

--- frontend/src/pages/PythonRequirements.vue
+++ frontend/src/pages/PythonRequirements.vue
@@ -39,12 +39,20 @@
             }
         },
         methods: {
+            /**
+             * Gets a list of issues from the requirements text input
+             */
             getIssues () {
                 this.getGithubFromPypi(this.requirementsText)
             },
+            /**
+             * Gets the list of github repos from the REST API
+             * @param requirementsText
+             */
             getGithubFromPypi (requirementsText)  {
                 this.$http.post('http://localhost:5000/api/pypi_github', { 'requirements_text': requirementsText }).then(result => {
                     let repos = result['body']
+                    // Get the github issues for each repo
                     for (var repoIndex in repos) {
                         let repo = repos[repoIndex]
                         this.getGithubIssues(repo['owner'], repo['package'])
@@ -54,14 +62,19 @@
                     console.log(errorResult)
                 })
             },
+            /**
+             * Gets the issues for each of the github repos directly from github
+             * @param owner
+             * @param project
+             */
             getGithubIssues (owner, project) {
                 if (owner.length === 0 || project.length === 0) {
                     return
                 }
                 this.$http.get('https://api.github.com/repos/' + owner + '/' + project + '/issues').then(result => {
                     if (result.status === 403) {
-                        this.githubError = false
-                        console.log('Getting github issues failed :(')
+                        // The github API has exceeded the maximum amount of requests
+                        this.githubError = true
                     } else if (result.status === 200) {
                         let newProject = {
                             "owner": owner,
@@ -75,6 +88,9 @@
                     console.log(errorResult)
                 })
             },
+            /**
+             * Closes the notification that the max github requests has been reached
+             */
             closeGithubError () {
                 this.githubError = false
             }
