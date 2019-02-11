e29fbc05390c9e83a41acac2dc05dc7fddf060e2
Mitch McAffee <themcaffee@gmail.com>
2019-02-11 03:34:30 -0700

    Get config from environment variables

--- config.py
+++ config.py
@@ -1,2 +1,9 @@
-ENV = "development"
-DEBUG = True
+import os
+
+# Attempt to get config from environment variables before default
+# for the production environment
+ENV = os.environ.get("ENV", default="development")
+DEBUG = os.environ.get("DEBUG", default=True)
+SECRET_KEY = os.environ.get("SECRET_KEY", default=None)
+if not SECRET_KEY:
+    SECRET_KEY = os.urandom(16)
